724059c3af6859f4e67af4d477b90caca7e994ad
MathianAkanati <mathian.akanati@loreal.com>
2025-02-28 14:01:53 +0000

    minor fix

--- SQL/dwh_msr/get_duplicated_sample.sql
+++ SQL/dwh_msr/get_duplicated_sample.sql
@@ -1,7 +1,8 @@
 SELECT
   {version}
   s.lkp_key_lma_sample,
-  COUNT(DISTINCT s.ak_key_msr_sample) AS nb_msr_sample
+  -- COUNT(DISTINCT s.ak_key_msr_sample) AS nb_msr_sample
+  COUNT(DISTINCT s.id_msr_sample) AS nb_msr_sample
 FROM
   `rni-sknow-gbl-ww-pd.sknow_bqdset_dwh_msr_eu_pd.tbl_dim_sample` s
 LEFT JOIN
